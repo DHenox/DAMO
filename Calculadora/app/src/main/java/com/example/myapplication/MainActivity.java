6777058c2ebc60df5191b48722be43a7acbe725e
DHenox <henokwav@gmail.com>
2022-10-03 20:32:24 +0200

    Primera entrega

--- Calculadora/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
